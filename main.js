dbc6072adf3320feaefbbd374f9394316caa9860
Ramachandra Ashok <71118901+ramachandra-ashok@users.noreply.github.com>
2021-05-10 07:40:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+//Select element function 
+const selectElement= function(element){
+    return document.querySelector(element);
+};
+
+let menuToggler= selectElement('.menu-toggle');
+let body= selectElement('body');
+
+
+menuToggler.addEventListener('click',function(){
+    body.classList.toggle('open');
+});
+
+//scroll reveal
+
+window.sr=ScrollReveal();
+
+sr.reveal('.animate-left',{
+    origin:'left',
+    duration:1500,
+    distance:'25rem',
+    delay:300
+});
+
+sr.reveal('.animate-right',{
+    origin:'right',
+    duration:1500,
+    distance:'25rem',
+    delay:300
+});
+
+sr.reveal('.animate-top',{
+    origin:'top',
+    duration:1500,
+    distance:'25rem',
+    delay:300
+});
+sr.reveal('.animate-bottom',{
+    origin:'bottom',
+    duration:1500,
+    distance:'25rem',
+    delay:300
+});
+
+var $buttons = jQuery('.nav-link');
+$buttons.on('click',function() {
+  jQuery(this).toggleClass('active').siblings('.nav-link').removeClass('active');
+})
